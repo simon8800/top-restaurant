329e036f5b130a85ee42003193e9b1d0926cd26a
Simon Mei <smei8800@gmail.com>
2024-05-08 12:02:38 -0400

    Add webpack-dev-server configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
@@ -13,5 +16,8 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true, // Cleans dist folder before each build, so that only used files will be generated
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
