2896efe2095c102edbbe08b23f81c5284df1b5a2
Simon Mei <smei8800@gmail.com>
2024-05-08 12:00:46 -0400

    Add output.clean true

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "main.js",
-      title: "TOP Restaurant",
       template: "./src/index.html",
     }),
   ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // Cleans dist folder before each build, so that only used files will be generated
+  },
 };
